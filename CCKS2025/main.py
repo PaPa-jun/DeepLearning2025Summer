aa3efcf3c5ebfc0767eb8ef98ff2ecc38feaa721
PaPa-jun <pengyufeng2003@outlook.com>
2025-05-30 11:35:02 +0800

    udpate

--- CCKS2025/main.py
+++ CCKS2025/main.py
@@ -1,4 +1,4 @@
-import torch
+import torch, time
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from utils import load_data, train_epoch, evaluate
@@ -11,8 +11,8 @@ texts, labels = load_data("train.jsonl", True)
 
 X_train, X_val, y_train, y_val = train_test_split(texts, labels, test_size=0.2, shuffle=True)
 
-tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-cased")
-model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-cased", num_labels=2)
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=2)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
@@ -24,10 +24,11 @@ val_set = TextDataset(X_val, y_val, tokenizer)
 train_loader = DataLoader(train_set, batch_size=32)
 val_loader = DataLoader(val_set, batch_size=32)
 
-for epoch in range(5):  # 训练 3 轮
+for epoch in range(1):  # 训练 3 轮
     train_loss = train_epoch(model, train_loader, optimizer, device)
     acc, precision, recall, f1 = evaluate(model, val_loader, device)
     print(f"Epoch {epoch+1} | Train Loss: {train_loss:.4f} | Val Acc: {acc:.4f} | F1: {f1:.4f}")
 
-model.save_pretrained("./best_model_distil")
-tokenizer.save_pretrained("./best_model_distil")
+local_time = time.localtime()
+model.save_pretrained(f"./checkpoints/{time.strftime('%Y%m%d%H%M%s', local_time)}")
+tokenizer.save_pretrained(f"./checkpoints/{time.strftime('%Y%m%d%H%M%s', local_time)}")
